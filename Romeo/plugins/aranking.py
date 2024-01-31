14842500fc4c8157bdfdecd7eb91fa898a123f06
Testromeo1 <134147817+Testromeo1@users.noreply.github.com>
2024-01-31 15:33:37 +0530

    Update aranking.py

--- Romeo/plugins/aranking.py
+++ Romeo/plugins/aranking.py
@@ -5,9 +5,6 @@ from datetime import datetime, timedelta
 from Romeo import app
 import congig
 
-# "Write a python code for telegram group message count all users message and  show  leader board top 10 users with command in pyrogram telegram bot and save all messages using mongodb_uri for all chat_id and add daily message count and overall message also should leader board and add a daily and overall button below leaderboard"     
-
-
 
 # Replace 'MONGODB_URI' with your MongoDB URI
 mongodb_uri = 'MONGO_URL'
