becb155a191116a1a1bd03944a0249ba8178a5aa
qadirnesirov02 <123129882+Qadirnesirov@users.noreply.github.com>
2024-03-18 23:52:51 +0400

    update basic.py

--- Romeo/helper/basic.py
+++ Romeo/helper/basic.py
@@ -2,7 +2,7 @@ from pyrogram.types import Message
 
 
 def get_user(message: Message, text: str) -> [int, str, None]:
-    """Get User From Message"""
+    """Mesajdan İstifadəçi Alın"""
     if text is None:
         asplit = None
     else:
@@ -30,7 +30,7 @@ def get_user(message: Message, text: str) -> [int, str, None]:
 
 
 def get_text(message: Message) -> [None, str]:
-    """Extract Text From Commands"""
+    """Əmrlərdən mətni çıxarın"""
     text_to_return = message.text
     if message.text is None:
         return None
